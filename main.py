a09af766a81714558a36bdcedf443a639da86156
ssjinkaido <53868217+ssjinkaido@users.noreply.github.com>
2021-09-19 16:36:42 +0700

    Update seed in main.py

--- main.py
+++ main.py
@@ -10,7 +10,19 @@ EPOCHS = 50
 LEARNING_RATE = 1e-3
 vision_transformer = VisionTransformer(image_size=32, num_classes=10)
 
+def seed_everything(seed):
+    import random, os
+    import numpy as np
+    import torch
 
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+    
 def main():
     for i in tqdm(range(EPOCHS)):
         model = vision_transformer.to(device)
@@ -18,4 +30,5 @@ def main():
         validate(model, val_loader, device)
 
 if __name__ == '__main__':
+    seed_everything(42)
     main()
