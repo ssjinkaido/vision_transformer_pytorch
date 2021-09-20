bed1ff0330d80591eb4fd6f5caf242bc27aae178
ssjinkaido <53868217+ssjinkaido@users.noreply.github.com>
2021-09-20 21:55:21 +0700

    Update train.py

--- train.py
+++ train.py
@@ -21,49 +21,51 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
 
-def train(model, epoch, learning_rate, train_loader, device):
-    optimizer = optim.Adagrad(model.parameters(), lr=learning_rate)
+def train(model, epoch, train_loader, optimizer, scaler, device):
+    
     criterion = nn.CrossEntropyLoss()
     batch_time = AverageMeter()
-    data_time = AverageMeter()
-    losses = AverageMeter()
-
     model.train()
     end = time.time()
+    train_loss = 0
+    correct = 0
+    total = 0
     for batch, (x, y) in enumerate(train_loader):
 
-        data_time.update(time.time() - end)
         x = x.to(device)
         y = y.to(device)
+        
         optimizer.zero_grad()
-        output = model(x)
+        with torch.cuda.amp.autocast(enabled=True):
+            output = model(x)            
+            loss = criterion(output, y)
 
-        loss = criterion(output, y)
-        loss.backward()
-        optimizer.step()
-
-        output = output.float()
-        loss = loss.float()
-        losses.update(loss.item(), x.size(0))
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+        
+        # loss.backward()
+        # optimizer.step()
+        train_loss += loss.item()
+        _, predicted = output.max(1)
+        total += y.size(0)
+        correct += predicted.eq(y).sum().item()
 
         batch_time.update(time.time() - end)
         end = time.time()
 
-        if batch % 2500 == 0:
-            print(
-                f'Epoch {epoch}, [{batch}/{len(train_loader)}], Time {batch_time.val:.3f} ({batch_time.avg:.3f}), Data {data_time.val:.3f} ({data_time.avg:.3f}), Loss {losses.val:.4f} ({losses.avg:.4f})')
-
-    return losses
+        if batch % 5000 == 0 and batch !=0:
+            print(f'Epoch {epoch}, [{batch}/{len(train_loader)}], Time {batch_time.avg:.3f}, Loss {train_loss/(batch+1)}, Acc {((correct/total)*100):.4f}')
 
 
-def validate(model, val_loader, device):
+
+def validate(model, val_loader, scheduler, device):
     criterion = nn.CrossEntropyLoss()
     batch_time = AverageMeter()
-    losses = AverageMeter()
-
-    # switch to evaluate mode
     model.eval()
-
+    test_loss = 0
+    correct = 0
+    total = 0
     end = time.time()
     with torch.no_grad():
         for batch, (x, y) in enumerate(val_loader):
@@ -73,15 +75,16 @@ def validate(model, val_loader, device):
             output = model(x)
             loss = criterion(output, y)
 
-            output = output.float()
-            loss = loss.float()
-
-            losses.update(loss.item(), x.size(0))
+            test_loss += loss.item()
+            _, predicted = output.max(1)
+            total += y.size(0)
+            correct += predicted.eq(y).sum().item()
+            
+            scheduler.step(test_loss)
 
             # measure elapsed time
             batch_time.update(time.time() - end)
             end = time.time()
 
-            if batch % 1250 == 0:
-                print(
-                    f'[{batch}/{len(val_loader)}], Time {batch_time.val:.3f} ({batch_time.avg:.3f}), Loss {losses.val:.4f} ({losses.avg:.4f})')
+            if batch % 5000 == 0 and batch !=0:
+                print(f'[{batch}/{len(val_loader)}], Time {batch_time.avg:.3f}, Loss {test_loss/(batch+1)}, Acc {((correct/total)*100):.4f}')
